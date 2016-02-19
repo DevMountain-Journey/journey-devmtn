34abebebb2ec51e77d8ad02b6819e2fc713e7e10
Gunnar Gabrielson <gunnargabrielson@gmail.com>
2016-02-19 12:07:14 -0700

    commented out run

---
+++ public/app/app.js
@@ -0,0 +1,13 @@
+angular.module('journey', [
+'ui.router'
+ ])
+
+
+.run(
+  // function($rootScope, errService){
+  //     $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error){
+  //       console.error(error);
+  //       errService.error(error);
+  //     });
+  //   }
+);
