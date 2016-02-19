722d53d33496292bdb0cd70480be0f17598fbbdd
Gunnar Gabrielson <gunnargabrielson@gmail.com>
2016-02-19 13:13:28 -0700

    Delete config.js

--- server/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    express: {
-        secret: '1324sa0923sf232'
-    },
-    port: 6312,
-    mongoUri: 'mongodb://dmjour:jH8EXXmMb6Z8ME5ddwaF@ds011238.mongolab.com:11238/journey'
-}
