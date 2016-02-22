f68327ace02282f96b4f4aff71bd05db3b50941a
Sam Miller <Samuelhaws@gmail.com>
2016-02-22 10:58:55 -0700

    merged

--- server/models/postsModel.js
+++ server/models/postsModel.js
@@ -2,13 +2,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var postsSchema = new Schema({
-    title: {type: 'String', required: true},
+//    title: {type: 'String', required: true},
 	body: {type: 'String'},
 	private: {type: 'Boolean'},
-	positiveScale: {type: 'Number', min: 1, max: 10},
+	positiveScale: {type: 'Number', min: 1, max: 10, required: true},
 	user: {type: Schema.Types.ObjectId, ref: 'Users'},
-	tags: [{type: 'String'}],
-	datePosted: {type: 'Date', required: true}
-})
+	tags: [{type: 'String', required: true}],
+	datePosted: {type: 'Date', required: true, default: Date.now}
+});
 
 module.exports =  mongoose.model('Posts', postsSchema);
