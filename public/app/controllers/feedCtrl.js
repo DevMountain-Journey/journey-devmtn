9ccf17fbafbf8b31466f37fdaf59e9cb9def729d
Gunnar Gabrielson <gunnargabrielson@gmail.com>
2016-02-25 12:17:18 -0700

    Fixed time since dates, cleaned up fixedPosts function

--- public/app/controllers/feedCtrl.js
+++ public/app/controllers/feedCtrl.js
@@ -3,22 +3,24 @@ angular.module('journey')
 .controller('feedCtrl',
   function($scope, $http, errService, postPromise, postService, auth, postCount, pageSize) {
     $scope.postContent = {};
-    $scope.posts = postPromise.data;
     $scope.totalPosts = postCount.data;
+    $scope.fixedPosts = []; //Init array to accept final posts object manipulation.
 
-    var groupedPosts = _.groupBy($scope.posts, function(post) {
-      return post.datePosted.substring(0, 10);
-    });
+    (function() {
+      //Take the resolve post data and use lodash to group by and convert dates to local time (from UTC).
+      var groupedPosts = _.groupBy(postPromise.data, function(post) {
+        post.datePosted = moment(post.datePosted).local().format("YYYY-MM-DDTHH:mm:ss");
+        return post.datePosted.substring(0, 10);
+      });
 
-    $scope.fixedPosts = [];
-
-    for (var date in groupedPosts) {
-      var obj = {};
-      obj.date = date;
-      obj.posts = groupedPosts[date];
-      $scope.fixedPosts.push(obj);
-    }
-
+      //Restructure the posts object to create an array of objects with the format we need in order to loop over the array with ng-repeat.
+      for (var date in groupedPosts) {
+        $scope.fixedPosts.push({
+          date: date,
+          posts: groupedPosts[date]
+        });
+      }
+    })();
 
     $scope.loadTags = function($query) {
       return $http.get('tags.json', {
@@ -36,27 +38,21 @@ angular.module('journey')
       $scope.postContent.tags = _.map($scope.postContent.tags, 'name');
       postService.createPost($scope.postContent)
         .then(function(response) {
-          if(response.status === 200){
+          if (response.status === 200) {
             $scope.postContent = {};
             $scope.totalPosts++;
             var newPost = response.data;
             newPost.user = {
-              _id : auth.data._id,
-              firstName : auth.data.firstName,
-              lastName : auth.data.lastName,
-              email : auth.data.email
+              _id: auth.data._id,
+              firstName: auth.data.firstName,
+              lastName: auth.data.lastName,
+              email: auth.data.email
             };
             //TODO: Below doesnt work always because if there are no posts for that day then it adds it to the previous day (whatever is on top of the array).
             $scope.fixedPosts[0].posts.unshift(newPost);
           } else {
             errService.error(response);
           }
-          // postService.getAllPost(pageSize.POSTS, $scope.currentPage)
-          //   .then(function(response) {
-          //     console.log('in getAllPosts');
-          //     console.log(response);
-          //     $scope.posts = response.data;
-          //   });
         }, function(err) {
           errService.error(err);
         });
@@ -106,18 +102,21 @@ angular.module('journey')
     };
 
     $scope.setScale = function(num) {
-        if ($scope.postContent.positiveScale === num + 1){
-            $scope.postContent.positiveScale = null;
-        }
-        else {
-            $scope.postContent.positiveScale = num + 1;
+      if ($scope.postContent.positiveScale === num + 1) {
+        $scope.postContent.positiveScale = null;
+      } else {
+        $scope.postContent.positiveScale = num + 1;
 
-        }
+      }
     };
 
 
     $scope.repeatEmotions = function() {
-        return new Array(10);
+      return new Array(10);
+    };
+
+    $scope.isToday = function(date) {
+      return moment(moment(date).format('YYYY-MM-DD')).isSame(moment().local().format('YYYY-MM-DD'));
     };
 
 
--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -1,15 +1,15 @@
 angular.module('journey')
   .controller('mainCtrl', function($scope, authService, $state) {
-    
+
   $scope.logout = function() {
          authService.logout()
          .then(function( response ) {
             console.log('in mainCtrl');
-            console.log('in logout')
+            console.log('in logout');
             console.log('response', response);
             if (response.status === 200) {
                 $state.go('login');
             }
-        }); 
-    }
+        });
+    };
 });
