0e273750848cb4784d6056e46f98ebedc7ed18bd
Sam Miller <Samuelhaws@gmail.com>
2016-02-28 19:00:10 -0700

    ok

---
+++ public/app/controllers/postCtrl.js
@@ -0,0 +1,44 @@
+angular.module('journey')
+.controller('postCtrl', function($stateParams, $scope, postService, auth) {
+   console.log($stateParams, "STATEPARAMS");
+
+ 
+ $scope.postData = postService.getOnePost($stateParams.id)
+.then(function(response) {
+            $scope.postData = response.data;   
+//             $scope.config = {
+//     title: 'Average Emotion',
+//     tooltips: true,
+//     labels: true,
+//     responsive:true,
+//     // legend: {
+//     //   display: false,
+//     //   //could be 'left, right'
+//     //   position: 'right',
+//     // },       
+//   };
+
+// $scope.data = {
+
+//   series: [$scope.postData.user.firstName, 'Average'],
+//   data: [{
+//       x: $scope.postData.user.lastName,
+//       y: [$scope.postData.positiveScale],
+//       tooltip: $scope.postData.user.firstName
+//         }, {
+//       x: "average",
+//       y: [3],
+//       tooltip: "Average"
+//     }],
+//   };    
+           
+          },
+          function(error) {
+            return error;
+          });
+   console.log($scope.postData, "postData"); 
+       
+       
+         
+    
+});
