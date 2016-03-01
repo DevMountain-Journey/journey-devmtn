c4fbe2c1e22ce5dda97eef14c2034c2cf1e5504f
Doug Alderman <agnadcp-d1@yahoo.com>
2016-03-01 14:14:56 -0700

    added date posted to comment model

--- server/models/commentsModel.js
+++ server/models/commentsModel.js
@@ -5,7 +5,8 @@ var commentsSchema = new Schema({
     body: {type: 'String', required: true},
     user: {type: Schema.Types.ObjectId, ref: 'Users'},
     postParent: {type: Schema.Types.ObjectId, ref: 'Posts'},
-    commentParent: {type: Schema.Types.ObjectId, ref: 'Comments'}
+    commentParent: {type: Schema.Types.ObjectId, ref: 'Comments'},
+    datePosted: {type: 'Date', default: Date.now}
 });
 
 module.exports =  mongoose.model('Comments', commentsSchema);
