587742a3e371fe2ff4391dd9f77b54fe27246d83
Gunnar Gabrielson <gunnargabrielson@gmail.com>
2016-03-06 19:42:36 -0700

    Email working!

--- server/models/commentsModel.js
+++ server/models/commentsModel.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose'),
     postsModel = require('./postsModel.js'),
     usersModel = require('./usersModel.js'),
+    nodemailer = require('nodemailer'),
+    sparkPostTransport = require('nodemailer-sparkpost-transport'),
     Schema = mongoose.Schema;
 
 var commentsSchema = new Schema({
@@ -10,6 +12,7 @@ var commentsSchema = new Schema({
     commentParent: {type: Schema.Types.ObjectId, ref: 'Comments'},
     datePosted: {type: 'Date', default: Date.now}
 });
+
 
 commentsSchema.post('save', function(comment){
   postsModel
@@ -24,8 +27,30 @@ commentsSchema.post('save', function(comment){
         .findById(post.user)
         .exec(function(err, user){
           if(err) return err;
-          if(user.preferences.communicationPreferences != ('none' || 'weeklySummary')){
-            console.log('YO DAWG, YOUR COMMUNICATION PREFERENCES IF STATEMENT IS WORKING PROPERLY!');
+          if(user.preferences && user.preferences.communicationPreferences != ('none' || 'weeklySummary')){
+            console.log('SENDING EMAIL');
+            var transporter = nodemailer.createTransport(sparkPostTransport({
+              "content": {
+                "template_id": "my-first-email"
+              },
+              "substitution_data": {
+                "postId": post._id
+              }
+            }));
+
+            transporter.sendMail({
+              "recipients": [
+                {
+                  "address": {
+                    "email": user.email,
+                    "name": user.firstName + " " + user.lastName
+                  }
+                }
+              ]
+            }, function(err, info) {
+              if (err) { console.log('EMAIL ERROR: ', err); }
+              else { console.log('EMAIL SENT: ', info); }
+            });
           }
           return comment;
         });
