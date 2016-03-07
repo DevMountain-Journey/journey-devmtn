8e1af07cf072e6d8e7dc0c1faf664c9dbe1ab8fa
Doug Alderman <agnadcp-d1@yahoo.com>
2016-03-07 11:25:51 -0700

    fix for uppercase login

--- server/controllers/passport.js
+++ server/controllers/passport.js
@@ -32,6 +32,9 @@ module.exports = function(passport) {
 
           // find a user whose email is the same as the forms email
           // we are checking to see if the user trying to login already exists
+          
+          // convert email to lowercase so that it matches 
+          email = email.toLowerCase();
           user.findOne({ 'email' :  email }, function(err, user) {
               // if there are any errors, return the error
               if (err) return done(err);
